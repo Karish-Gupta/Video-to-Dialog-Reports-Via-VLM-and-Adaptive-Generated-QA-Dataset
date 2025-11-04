46653016bcb3d93ac0aca58ab940fb09672d01f2
Karish Gupta <karishgupta2003@gmail.com>
2025-11-03 22:31:24 -0500

    LLM needs to decode new gen tokens

--- VLM/qa_generation.py
+++ VLM/qa_generation.py
@@ -51,9 +51,10 @@ class llm:
       inputs = self.tokenizer(prompt, 
                               return_tensors="pt", 
                               truncation=True, 
-                              max_length=10240
+                              max_length=8192
                               ).to(self.model.device)
-      outputs = self.model.generate(**inputs, max_new_tokens=256, temperature=1)
-      decoded_output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+      outputs = self.model.generate(**inputs, max_new_tokens=256, temperature=1.0)
+      gen_tokens = outputs[0][inputs["input_ids"].shape[1]:]
+      decoded_output = self.tokenizer.decode(gen_tokens, skip_special_tokens=True)
       return decoded_output
 
