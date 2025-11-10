e541187484c459247aecdf5e3903ad4d68bf1835
Alex Li <alexlibaobao@gmail.com>
2025-11-10 12:46:08 -0500

    Support Llava 34B

--- VLM/run_vlm.sh
+++ VLM/run_vlm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -N 1                          # allocate 1 compute node
 #SBATCH -n 1                          # total number of tasks
-#SBATCH --mem=64g                     # allocate 32 GB of memory
+#SBATCH --mem=128g                     # allocate 32 GB of memory
 #SBATCH -J "vlm_inference"              # name of the job
 #SBATCH -o vlm_inference_%j.out         # name of the output file
 #SBATCH -e vlm_inference_%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
 #SBATCH -t 5:00:00                   # time limit of 12 hours
-#SBATCH --gres=gpu:H100:2             # request 1 H200 GPU
+#SBATCH --gres=gpu:H100:1             # request 1 H100 GPU
 
 cd $SLURM_SUBMIT_DIR/..
 
@@ -19,7 +19,7 @@ source env/bin/activate
 
 pip install --upgrade pip
 pip install --upgrade -q accelerate bitsandbytes
-pip install git+https://github.com/huggingface/transformers.git
+pip install transformers==4.45.0
 pip install huggingface-hub
 pip install -q av
 pip install pillow
