ff5f145935ca8ed0dae982e8301b93a4da7886d3
Matthew Alex <matthewalex2005@gmail.com>
2025-11-11 01:11:41 -0500

    quick fix

--- newPipeline/run_pipeline.sh
+++ newPipeline/run_pipeline.sh
@@ -28,5 +28,6 @@ pip install protobuf
 pip install sentencepiece
 pip install torchcodec
 pip install decord==0.6.0
+pip install -r requirements.txt
 
 python newPipeline/run_pipeline.py "https://www.youtube.com/watch?v=aNXf82gWb5A"
