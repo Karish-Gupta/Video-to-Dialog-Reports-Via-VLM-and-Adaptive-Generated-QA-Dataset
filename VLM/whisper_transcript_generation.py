0bb9f5f5285df481c94e290146a3b847df4fbe28
Karish-Gupta <karishgupta2003@gmail.com>
2025-11-17 00:31:32 -0500

    updates

--- VLM/whisper_transcript_generation.py
+++ VLM/whisper_transcript_generation.py
@@ -1,9 +1,11 @@
 import os
 import whisperx
 from whisperx.diarize import DiarizationPipeline
+from huggingface_hub import login
 
-hf_token = os.environ.get("HF_TOKEN", None) # Huggingface token
-
+if "HF_TOKEN" in os.environ:
+   login(token=os.environ["HF_TOKEN"])
+   
 VIDEO_DIR = "VLM/copa_videos"
 OUTPUT_DIR = "VLM/whisper_transcripts"
 
--- VLM/whisper_transcript_generation.sh
+++ VLM/whisper_transcript_generation.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1                          # allocate 1 compute node
 #SBATCH -n 1                          # total number of tasks
 #SBATCH --mem=128g                     # allocate 128 GB of memory
-#SBATCH -J "whsiper_transcript_generation"              # name of the job
-#SBATCH -o whsiper_transcript_generation%j.out         # name of the output file
-#SBATCH -e whsiper_transcript_generation%j.err         # name of the error file
+#SBATCH -J "whisper_transcript_generation"              # name of the job
+#SBATCH -o whisper_transcript_generation%j.out         # name of the output file
+#SBATCH -e whisper_transcript_generation%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
 #SBATCH -t 5:00:00                   # time limit of 12 hours
 #SBATCH --gres=gpu:H100:1             
