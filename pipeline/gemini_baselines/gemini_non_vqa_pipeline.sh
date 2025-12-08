8c5f56e6822922f84f962e359e153eae922aef7a
Matthew Alex <matthewalex2005@gmail.com>
2025-12-08 16:54:18 -0500

    gemini restructuring

--- pipeline/gemini_baselines/gemini_non_vqa_pipeline.sh
+++ pipeline/gemini_baselines/gemini_non_vqa_pipeline.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1                          # allocate 1 compute node
 #SBATCH -n 1                          # total number of tasks
 #SBATCH --mem=128g                     # allocate 128 GB of memory
-#SBATCH -J "gemini_vqa_pipeline"              # name of the job
+#SBATCH -J "gemini_non_vqa_pipeline"              # name of the job
 #SBATCH -o gemini_non_vqa_pipeline%j.out         # name of the output file
-#SBATCH -e gemini_vqa_pipeline%j.err         # name of the error file
+#SBATCH -e gemini_non_vqa_pipeline%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
 #SBATCH -t 1:00:00                   # time limit of 12 hours
 
