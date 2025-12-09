289c88f744adedebb8f981222d75995474e93d97
Matthew Alex <matthewalex2005@gmail.com>
2025-12-09 12:37:15 -0500

    fix

--- pipeline/gemini_baselines/gemini_non_vqa_pipeline.py
+++ pipeline/gemini_baselines/gemini_non_vqa_pipeline.py
@@ -21,7 +21,7 @@ def extract_generated_text_vlm(raw_output: str):
 
 # CONFIG
 VIDEO_DIR = "pipeline/copa_videos"
-TRANSCRIPT_DIR = "pipeline/whisper_transcripts"
+TRANSCRIPT_DIR = "pipeline/whisper_transcripts_diarize"
 OUTPUT_DIR = "pipeline/gemini_non_vqa"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 gemini = gemini_model()
